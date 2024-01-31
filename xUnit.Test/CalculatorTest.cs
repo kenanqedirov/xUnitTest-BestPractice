7e737e87b73ea8c4a1cb33fc3794947b29a9c0e0
kenanqedirov <k.qedirov1999@gmail.com>
2024-01-31 19:39:16 +0400

    Add project files.

---
+++ xUnit.Test/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.APP;
+using Xunit;
+
+namespace xUnit.Test
+{
+
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange
+            int a= 5 ;
+            int b= 20;
+            var calculator = new Calculator();
+            //Act
+            var total = calculator.Add(a,b);
+            //Assert
+            Assert.NotEqual<int>(26,total); 
+        }
+    }
+} 
